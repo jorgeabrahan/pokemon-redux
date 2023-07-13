0a2bfa863a1458d4654a0d1f5e43ee2ffe1f7e49
jorgeabrahan <jorge24abrahan@gmail.com>
2023-07-13 00:34:43 -0600

    Create pokemon app component to display pages of pokemons with a limit of 10 elements

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,13 +1,35 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getPokemons } from './store/slices/pokemon'
+
 export const PokemonApp = () => {
+  const dispatch = useDispatch()
+  const { page, pokemons, isLoading } = useSelector((store) => store.pokemons)
+
+  const handleNextPage = () => dispatch(getPokemons(page))
+
+  useEffect(() => {
+    dispatch(getPokemons())
+  }, [])
+
   return (
     <>
       <h1>PokemonApp</h1>
+      {isLoading && <p>Loading...</p>}
       <hr />
       <ul>
-        <li>Hola</li>
-        <li>Hola</li>
-        <li>Hola</li>
+        {pokemons.map(({ name }) => (
+          <li key={name}>{name}</li>
+        ))}
       </ul>
+      {!isLoading && (
+        <p>
+          {(page - 1) * 10} - {page * 10}
+        </p>
+      )}
+      <button disabled={isLoading} onClick={handleNextPage}>
+        next
+      </button>
     </>
   )
 }
